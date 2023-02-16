a5d9eb3f38ed04746dab8d2199df070ec4c2471a
Rizky Benggolo <travarilo@gmail.com>
2023-02-16 17:36:49 +0700

    cleanup NavigationsSettings

--- src/org/lineageos/setupwizard/NavigationSettingsActivity.java
+++ src/org/lineageos/setupwizard/NavigationSettingsActivity.java
@@ -20,8 +20,6 @@ import static android.view.WindowManagerPolicyConstants.NAV_BAR_MODE_2BUTTON_OVE
 import static android.view.WindowManagerPolicyConstants.NAV_BAR_MODE_3BUTTON_OVERLAY;
 import static android.view.WindowManagerPolicyConstants.NAV_BAR_MODE_GESTURAL_OVERLAY;
 
-import static org.lineageos.internal.util.DeviceKeysConstants.KEY_MASK_APP_SWITCH;
-import static org.lineageos.setupwizard.SetupWizardApp.DISABLE_NAV_KEYS;
 import static org.lineageos.setupwizard.SetupWizardApp.NAVIGATION_OPTION_KEY;
 
 import android.animation.Animator;
@@ -40,8 +38,6 @@ import android.widget.RadioGroup.OnCheckedChangeListener;
 import com.airbnb.lottie.LottieAnimationView;
 import com.google.android.setupcompat.util.WizardManagerHelper;
 
-import lineageos.providers.LineageSettings;
-
 import org.lineageos.setupwizard.util.SetupWizardUtils;
 
 public class NavigationSettingsActivity extends BaseSetupWizardActivity {
@@ -52,21 +48,11 @@ public class NavigationSettingsActivity extends BaseSetupWizardActivity {
 
     private String mSelection = NAV_BAR_MODE_GESTURAL_OVERLAY;
 
-    private CheckBox mHideGesturalHint;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         mSetupWizardApp = (SetupWizardApp) getApplication();
-        boolean navBarEnabled = false;
-        if (mSetupWizardApp.getSettingsBundle().containsKey(DISABLE_NAV_KEYS)) {
-            navBarEnabled = mSetupWizardApp.getSettingsBundle().getBoolean(DISABLE_NAV_KEYS);
-        }
-
-        int deviceKeys = getResources().getInteger(
-                org.lineageos.platform.internal.R.integer.config_deviceHardwareKeys);
-        boolean hasHomeKey = (deviceKeys & KEY_MASK_APP_SWITCH) != 0;
 
         getGlifLayout().setDescriptionText(getString(R.string.navigation_summary));
         setNextText(R.string.next);
@@ -90,9 +76,8 @@ public class NavigationSettingsActivity extends BaseSetupWizardActivity {
         }
 
 
-        // Hide this page if the device has hardware keys but didn't enable navbar
-        // or if there's <= 1 available navigation modes
-        if (!navBarEnabled && hasHomeKey || available <= 1) {
+        // Hide this page if there's <= 1 available navigation modes
+        if (available <= 1) {
             mSetupWizardApp.getSettingsBundle().putString(NAVIGATION_OPTION_KEY,
                     NAV_BAR_MODE_3BUTTON_OVERLAY);
             Intent intent = WizardManagerHelper.getNextIntent(getIntent(), Activity.RESULT_OK);
@@ -102,7 +87,6 @@ public class NavigationSettingsActivity extends BaseSetupWizardActivity {
         final LottieAnimationView navigationIllustration =
                 findViewById(R.id.navigation_illustration);
         final RadioGroup radioGroup = findViewById(R.id.navigation_radio_group);
-        mHideGesturalHint = findViewById(R.id.hide_navigation_hint);
         radioGroup.setOnCheckedChangeListener(new OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
@@ -111,17 +95,14 @@ public class NavigationSettingsActivity extends BaseSetupWizardActivity {
                     mSelection = NAV_BAR_MODE_GESTURAL_OVERLAY;
                     navigationIllustration
                             .setAnimation(R.raw.lottie_system_nav_fully_gestural);
-                    revealHintCheckbox();
                     break;
                 case R.id.radio_two_button:
                     mSelection = NAV_BAR_MODE_2BUTTON_OVERLAY;
                     navigationIllustration.setAnimation(R.raw.lottie_system_nav_2_button);
-                    hideHintCheckBox();
                     break;
                 case R.id.radio_sw_keys:
                     mSelection = NAV_BAR_MODE_3BUTTON_OVERLAY;
                     navigationIllustration.setAnimation(R.raw.lottie_system_nav_3_button);
-                    hideHintCheckBox();
                     break;
                 }
 
@@ -130,45 +111,9 @@ public class NavigationSettingsActivity extends BaseSetupWizardActivity {
         });
     }
 
-    private void revealHintCheckbox() {
-        mHideGesturalHint.animate().cancel();
-
-        if (mHideGesturalHint.getVisibility() == View.VISIBLE) {
-            return;
-        }
-
-        mHideGesturalHint.setVisibility(View.VISIBLE);
-        mHideGesturalHint.setAlpha(0.0f);
-        mHideGesturalHint.animate()
-            .translationY(0)
-            .alpha(1.0f)
-            .setListener(null);
-    }
-
-    private void hideHintCheckBox() {
-        if (mHideGesturalHint.getVisibility() == View.INVISIBLE) {
-            return;
-        }
-
-        mHideGesturalHint.animate()
-            .translationY(-mHideGesturalHint.getHeight())
-            .alpha(0.0f)
-            .setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    super.onAnimationEnd(animation);
-                    mHideGesturalHint.setVisibility(View.INVISIBLE);
-                }
-            });
-    }
-
     @Override
     protected void onNextPressed() {
         mSetupWizardApp.getSettingsBundle().putString(NAVIGATION_OPTION_KEY, mSelection);
-        boolean hideHint = mHideGesturalHint.isChecked();
-        LineageSettings.System.putIntForUser(getContentResolver(),
-                LineageSettings.System.NAVIGATION_BAR_HINT, hideHint ? 0 : 1,
-                UserHandle.USER_CURRENT);
         Intent intent = WizardManagerHelper.getNextIntent(getIntent(), Activity.RESULT_OK);
         nextAction(NEXT_REQUEST, intent);
     }
